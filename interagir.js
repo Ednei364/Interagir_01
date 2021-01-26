d0a5e34ed99748d02007d296a21ea384326f91d5
Ednei Vieira <ednei.gois167@gmail.com>
2021-01-26 00:04:28 -0400

    ajuste5

--- interagir.js
+++ interagir.js
@@ -3,23 +3,29 @@ var saldacao = confirm(`Olá ${nome}, tudo bem contigo?`)
 saldacao == true ? alert(`Que bom ${nome}`):alert(` Que pena meu amigo,${nome}`)
 
  var nasc = prompt(`${nome} em que ano voce nasceu?`)
- nasc1 = parseInt(nasc)
+ nasc1 = parseFloat(nasc)
  var tipo = typeof(nasc1)
  now = new Date
  var Year = now.getFullYear()
  var idade = Year - nasc1
- if(tipo == "number"){
+ if(isNaN(nasc1)){
+    alert(`Dados informado não é equivalente a um número, favor recarregar a página e tente novamente`)
+    }else{
      if(nasc1 > 1950){
          if(nasc1 < 2021){
-             alert(`Este ano de ${Year}, você irar completar ${idade} anos`)
+             if(nasc1 == 2020){
+                alert(`Este ano de ${Year}, você irar completar seu primeiro ano de vida.`)
+             }else{
+                alert(`Este ano de ${Year}, você irar completar ${idade} anos`)
+             }             
          }else{
              alert(`Informe um ano inferior a 2021, favor recarregar a página e tente novamente`)
          }
      }else{
          alert(`Informe um ano superio a 1950, favor recarregar a página e tente novamente`)
      }
- }else{
-     alert(`Dados informado não é equivalente a um número, favor recarregar a página e tente novamente`)
+ var agradecimento = alert(`Obrigado por participar ${nome}, até a proxima meu amigo. `)
+     
  }
 var button = document.querySelector('[data-js="button"]');
 
