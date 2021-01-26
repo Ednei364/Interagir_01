917d827fd222cb518e11ae31aca1648b2d35fadd
Ednei Vieira <ednei.gois167@gmail.com>
2021-01-25 23:17:32 -0400

    ajuste2

--- interagir.js
+++ interagir.js
@@ -1,25 +1,25 @@
-var nome = prompt("Qual seu nome? ");
-var saldacao = confirm(`Olá ${nome}, tudo bem contigo?`)
-saldacao == true ? alert(`Que bom ${nome}`):alert(` Que pena meu amigo,${nome}`)
+// var nome = prompt("Qual seu nome? ");
+// var saldacao = confirm(`Olá ${nome}, tudo bem contigo?`)
+// saldacao == true ? alert(`Que bom ${nome}`):alert(` Que pena meu amigo,${nome}`)
 
-var nasc = prompt(`${nome} em que ano voce nasceu?`)
-var tipo = typeof(nasc)
-now = new Date
-var Year = now.getFullYear()
-var idade = Year - nasc
-if(tipo == "number"){
-    if(nasc1 > 1950){
-        if(nasc1 < 2021){
-            alert(`Este ano de ${Year}, você irar completar ${idade} anos`)
-        }else{
-            alert(`Informe um ano inferior a 2021, favor recarregar a página e tente novamente`)
-        }
-    }else{
-        alert(`Informe um ano superio a 1950, favor recarregar a página e tente novamente`)
-    }
-}else{
-    alert(`Dados informado não é equivalente a um número, favor recarregar a página e tente novamente`)
-}
+// var nasc = prompt(`${nome} em que ano voce nasceu?`)
+// var tipo = typeof(nasc)
+// now = new Date
+// var Year = now.getFullYear()
+// var idade = Year - nasc
+// if(tipo == "number"){
+//     if(nasc1 > 1950){
+//         if(nasc1 < 2021){
+//             alert(`Este ano de ${Year}, você irar completar ${idade} anos`)
+//         }else{
+//             alert(`Informe um ano inferior a 2021, favor recarregar a página e tente novamente`)
+//         }
+//     }else{
+//         alert(`Informe um ano superio a 1950, favor recarregar a página e tente novamente`)
+//     }
+// }else{
+//     alert(`Dados informado não é equivalente a um número, favor recarregar a página e tente novamente`)
+// }
 var button = document.querySelector('button');
 
 button.onclick = function() {
